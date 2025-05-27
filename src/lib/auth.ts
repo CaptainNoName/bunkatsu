f8beededecd63f9d0920facee64bff2b81dbe55e
Szymon <szymon@mac-1.home>
2025-05-27 21:24:00 +0200

    chore: use env instead of process.env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '@/db'
+import { env } from '@/env'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -11,8 +12,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     },
   },
 })
